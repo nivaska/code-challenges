f7639d477e15d273e1fe8f40312207173a3b0227
nivas619ka <nivas619ka@gmail.com>
2023-12-07 22:50:01 +0100

    foobar solved task 3

--- google foobar 2024/FooBarApp/Program.cs
+++ google foobar 2024/FooBarApp/Program.cs
@@ -5,5 +5,8 @@ Console.WriteLine("Task 2: Bunny Worker Locations");
 Console.WriteLine(Task2.Solution.Run(5, 10));
 Console.WriteLine("----------------------");
 
+Console.WriteLine("Task 3: Power Hungry");
+Console.WriteLine(Task3.Solution.Run([-2, -3, 4, -5]));
+Console.WriteLine("----------------------");
 
 Console.ReadKey();
--- google foobar 2024/FooBarApp/Task3/Solution.cs
+++ google foobar 2024/FooBarApp/Task3/Solution.cs
@@ -4,7 +4,42 @@ public class Solution
 {
     public static int Run(int[] x)
     {
+        int prod_pos = 1, prod_neg = 1;
+        int count_pos = 0, count_neg = 0, count_zero = 0;
+        int largest_neg = 1;
 
-        return 0;
+        foreach (var num in x)
+        {
+            if (num > 0){
+                count_pos++;
+                prod_pos *= num;
+            } 
+            else if (num < 0) 
+            {
+                count_neg++;
+                prod_neg *= num;
+
+                if(num > largest_neg || largest_neg == 1)
+                {
+                    largest_neg = num;
+                }
+            }
+            else
+            {
+                count_zero++;
+            }
+        }
+
+        if (count_pos is 0 && count_zero is not 0 && count_neg is 1 or 0)
+        {
+            return 0;
+        }
+
+        if (count_pos is 0 && count_zero is 0 && count_neg is 1)
+        {
+            return prod_neg;
+        }
+
+        return prod_pos * prod_neg / (prod_neg < 0 ? largest_neg: 1);
     }
 }
